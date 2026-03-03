104c23c34218df2ca7f2d704398c3be8ab229094
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-03-03 12:18:44 +0100

    rajout des service et des affichage des employe avec service

--- jour2/jobs7.py
+++ jour2/jobs7.py
@@ -11,5 +11,15 @@ cursor.execute("SELECT * FROM employe WHERE salaire > 3000")
 results=cursor.fetchall()
 print(results)
 
+cursor.execute( """
+SELECT employe.id, employe.nom, employe.prenom, employe.salaire, service.nom
+FROM employe
+INNER JOIN service ON employe.id_service = service.id
+""")
+
+results=cursor.fetchall()
+for row in results:
+    print(row)
+
 cursor.close()
 db.close()
