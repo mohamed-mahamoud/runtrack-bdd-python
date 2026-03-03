126557335b542c752cade6f823a4912bc04e10da
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-03-03 11:29:28 +0100

    rajout du job6

---
+++ jour2/jobs6.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+db= mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="MohamedSwain-13010",
+    db="Laplateforme",)
+
+cursor=db.cursor()
+cursor.execute("SELECT capacite FROM salle")
+results=cursor.fetchall()
+capacite_totale = sum(row[0] for row in results)
+
+print(f"Capacité totale des salles est de : {capacite_totale}")
+
+cursor.close()
+db.close()
